0c23188eafb40ebfe764deb67427286182a3b24d
Annalisa <annalisadesantis770@gmail.com>
2020-10-22 10:06:27 +0200

    commenti due varianti

--- palindroma/main.js
+++ palindroma/main.js
@@ -31,18 +31,19 @@ function checkPalindrom(str) {
 
 // Soluzione 2 ------------------------------
 
-
+// // Creo un array per leggere le singole stringhe una alla volta
 // for(var i = 0; i < nomi.length; i++) {
 //     var nome_corrente = nomi[i];
 //     console.log(nome_corrente);
 //
+// // Creo una varibile per mettere le singole lettere da leggere
 //     var parole = "";
-//
+// // Creo un ciclo for per leggere la singola stringa da destra verso sinistra quando legge la lettera la mette nella varibile parola (tipo somma)
 //     for (var y = nome_corrente.length - 1; y >= 0; y--) {
 //         var nome_corrente_invertito = nome_corrente[y];
 //         parole += nome_corrente_invertito;
 //     }
-//
+// // Creo un if per verificare se la parola corrente che stiamo leggendo nella stringa è uguale alla parola letta da destra verso sinistra (varibile parola)
 //     if(nome_corrente == parole){
 //         console.log("Il nome " + nome_corrente + " è palindromo");
 //     }else{
@@ -52,14 +53,16 @@ function checkPalindrom(str) {
 // }
 
 
-// Soluzione 3 con prompt---------------------
-//
+// // Soluzione 3 con prompt---------------------
+// // Creo una funzione per verificare che l'elemeto che vogliamo quando lo invertiamo è uguale alla parola iniziale
 // function checkPalindrom(str){
 //     return (str == str.split('').reverse().join(''));
 // }
-//
+// // Chiedo all'utente di inserire una parola
 // var nome_utente = prompt("Inserisci una parola");
+// // Creo una varibile di supporto dove il risultato deve essere vero quando la funzione è vera (ovvero quando la parola inserita dall'utente soddisfa i requisiti di return della funzione)
 // var risultato = checkPalindrom(nome_utente);
+// // Creo un if per verificare se è vera è palindroma se è falsa non è palindroma
 // if (risultato){
 //     console.log("La parola che hai inserito è palindroma");
 // }else{
